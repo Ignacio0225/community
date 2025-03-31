792438f181138b819c985226f6446c4891720e19
유선혁 <tjsgur2399@icloud.com>
2025-03-31 21:39:52 +0900

    Initial commit

---
+++ users/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = ('male','Male')
+        FEMALE = ('female','Female')
+    class LanguageChoices(models.TextChoices):
+        KR = ('kr', 'Korean')
+        EN = ('en', 'English')
+        ES = ('es', 'Español')
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    avatar = models.URLField(
+        blank=True,
+    )
+    name = models.CharField(
+        max_length=150,
+        default=""
+    )
+    is_host = models.BooleanField(
+        default=False,
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices,
+    )
